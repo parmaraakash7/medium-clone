0c060149edad6c338edfe3c9eddabea9831743d9
parmaraakash7 <parmaraakash783@gmail.com>
2021-10-27 13:48:39 +0530

    added atlas

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-
-mongoose.connect('mongodb://127.0.0.1:27017/medium_clone');
+const uri = "mongodb+srv://aakash_cluster_1:aakash_cluster_1@cluster1.ifsva.mongodb.net/medium_clone?retryWrites=true&w=majority";
+/*mongoose.connect('mongodb://127.0.0.1:27017/medium_clone');
 
 const db = mongoose.connection;
 db.once("open", function () {
   console.log("Connected successfully");
-});
+});*/
+
+try {
+    // Connect to the MongoDB cluster
+     mongoose.connect(
+      uri,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      () => console.log(" Mongoose is connected")
+    );
+
+  } catch (e) {
+    console.log("could not connect");
+  }
